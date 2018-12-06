5bc6536060a1c88cea705c3d5544db7c390afd03
usuario021 <45642769+usuario021@users.noreply.github.com>
2018-12-06 19:14:54 -0300

    Update ListaUsuarios.java

--- src/tienditamusic/ListaUsuarios.java
+++ src/tienditamusic/ListaUsuarios.java
@@ -15,57 +15,67 @@ import java.util.*;
  */
 public class ListaUsuarios{
     ArrayList <Usuario> listaUsuarios;
+	
     public ListaUsuarios() {
-		listaUsuarios = new ArrayList <Usuario> ();		
+	listaUsuarios = new ArrayList <Usuario>();		
+    }
+	
+    public boolean agregarUsuario(Usuario u){
+	if (buscarUsuario(u)){
+            return false;
 	}
-    public boolean agregarUsuario(Usuario u) {
-		listaUsuarios.add(u);
-		return false;
-		
+	listaUsuarios.add(u);
+	return true;
+    }
+    public void agregarUsuario(String user, String pass, int id, int tipo){
+        Usuario u = new Usuario(user, pass, id, tipo);
+	if (buscarUsuario(u)){
+	    return false;
 	}
-    public void agregarUsuario(String user,String pass,int id,int tipo){
-        Usuario u = new Usuario(user,pass,id,tipo);
         listaUsuarios.add(u);
     }
-	public Usuario eliminarUsuario (Usuario u) {
-		for(int i=0;i<listaUsuarios.size();i++) {
-			 if(listaUsuarios.get(i).getIdUsuario()==u.getIdUsuario()) {
-				 return listaUsuarios.remove(i);
-			 }
-		}
-		return null;
+    public Usuario eliminarUsuario (Usuario u){
+	int i = BuscarIdUsuario(u.getIdUsuario);
+	if (i != -1){
+	    return listaUsuarios.remove(i);
 	}
-        public boolean editarUsuario (Usuario u) {
-		for(int i=0;i<listaUsuarios.size();i++) {
-			if(listaUsuarios.get(i).getIdUsuario()== u.getIdUsuario()) {
-				listaUsuarios.remove(i);
-				listaUsuarios.add(u);
-				return true;
-			}
-		}
-		return false;
+        return null;
+    }					
+    public boolean editarUsuario (Usuario u){
+	for(int i=0; i<listaUsuarios.size(); i++) {
+	    if(listaUsuarios.get(i).getIdUsuario()== u.getIdUsuario()) {
+		listaUsuarios.remove(i);
+		listaUsuarios.add(u);
+		return true;
+	    }
 	}
-        public Usuario buscarUsuario (String user){
-            for (int i=0; i<listaUsuarios.size();i++){
-                if(listaUsuarios.get(i).getNombreUsuario().equals(user)){
-                    return listaUsuarios.get(i);
-                }               
-            }
-             return null;
+	return false;
+    }
+    public Usuario buscarUsuario (String user){
+    	for(int i=0; i<listaUsuarios.size(); i++){
+      	    if(listaUsuarios.get(i).getNombreUsuario().equals(user)){
+	        return listaUsuarios.get(i);
+      	    }               
+  	}
+        return null;
+    }
+    public boolean buscarUsuario(Usuario u){
+        for(int i=0; i<listaUsuarios.size(); i++){
+	    if(listaUsuarios.get(i).equals(u)){
+	        return true;
+	    }
         }
-        
-        public boolean buscarUsuario(Usuario u){
-            for(int i=0; i<listaUsuarios.size();i++){
-                if(listaUsuarios.get(i).equals(u)==true){
-                    return true;
-                }
-            }
-            return false;
-        }
-        public Usuario getUser(int i){
-            return listaUsuarios.get(i);
-        }
-    
-        
-         
-        }
+        return false;
+    }
+    public Usuario getUser(int i){
+        return listaUsuarios.get(i);
+    }
+    private int BuscarPosicion(int Id){
+        for(int i; i< listaUsuarios.size(); i++){
+	    if(listaUsuarios.get(i).getIdUsuario() == Id){
+	        return i;
+	    }
+        return -1;    
+    }
+} 
+            
