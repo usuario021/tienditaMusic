8666767daa8862f3d2e2c6a1e3657fde0ecf50de
TomasRojas <rojas.daydi@gmail.com>
2018-12-07 16:17:29 -0300

    Add files via upload

---
+++ src/tienditamusic/LoginController.java
@@ -0,0 +1,50 @@
+package tienditamusic;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author rojas
+ */
+public class LoginController implements Initializable {
+    
+    ListaUsuarios model;
+    
+    @FXML
+    private TextField username;
+    
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    public void logear(ActionEvent event) throws IOException{
+        String nombre = username.getText();
+        String pass = password.getText();
+        if(nombre.equals("admin")&&pass.equals("1234")){
+            Parent parent = FXMLLoader.load(getClass().getResource("AdminMenu.fxml"));
+            Scene escenaMenu = new Scene(parent);
+            Stage menuStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            menuStage.setScene(escenaMenu);
+            menuStage.show();
+        }
+        else{
+            AlertBox.mostrarPopUp("Credenciales Incorrectas", "Nombre o contraseña incorrectos, por favor vuelva a iniciar sesion");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
