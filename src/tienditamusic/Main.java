61f11d403f72e3f17c86b7f5626b351e9e402ea5
TomasRojas <rojas.daydi@gmail.com>
2018-12-07 16:16:20 -0300

    Update Main.java

--- src/tienditamusic/Main.java
+++ src/tienditamusic/Main.java
@@ -1,23 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tienditamusic;
-/**
- *
- * @author EDU
- */
-import java.io.*;
-import java.util.*;
-public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    
+    Stage window;
+    
     public static void main(String[] args) {
-        TiendaMusica tiendita = new TiendaMusica();
-        VentanaLogin login = new VentanaLogin (tiendita);
-        login.setVisible(true);   
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            salir();
+            });
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    private void salir(){
+        boolean respuesta = ConfirmarAccion.mostrarPopUp("¿Desea salir?", "Está seguro que desea salir?");
+        if(respuesta)
+            window.close();
     }
 }
